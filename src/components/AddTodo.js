b23088f70f22840337f0cc6f61ba4e792d148062
Mike <m_kanyar@encs.concordia.ca>
2021-07-12 22:01:40 -0400

    AddTodo component

---
+++ src/components/AddTodo.js
@@ -0,0 +1,15 @@
+import React from "react";
+
+export const AddToDo = () => {
+  return (
+    <div className="addTodoContainer">
+      {" "}
+      <h1>TodoList</h1>
+      <div className="addTodo">
+        <input type="text" />
+
+        <button className="addButton">Add to do</button>
+      </div>
+    </div>
+  );
+};
