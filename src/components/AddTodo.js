f713e12d205606642732c77c400eb3109ddf6a6f
Mike <m_kanyar@encs.concordia.ca>
2021-07-15 00:27:23 -0400

    fix clear input

--- src/components/AddTodo.js
+++ src/components/AddTodo.js
@@ -1,17 +1,18 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import { connect } from "react-redux";
 import { addTodo } from "../redux/actions";
 
 const AddToDo = ({ addTodo }) => {
   const [input, setInput] = useState("");
+
   const handleChange = (event) => {
     setInput(event.target.value);
   };
+
   const handleAdd = (event) => {
     addTodo(input);
-
-    console.log("added ", input);
     setInput("");
+    console.log("added ", input);
   };
 
   return (
@@ -19,13 +20,8 @@ const AddToDo = ({ addTodo }) => {
       {" "}
       <h1 className="title">TodoList</h1>
       <div className="addTodo">
-        <input type="text" onChange={handleChange} />
-        <button
-          className="addButton"
-          type="reset"
-          onClick={handleAdd}
-          value={input}
-        >
+        <input type="text" onChange={handleChange} value={input} />
+        <button className="addButton" onClick={handleAdd}>
           Add to do
         </button>
       </div>
